38ded532273b31c6b123fca953d85cd959e61c37
m-aljamal <mohammadjamol@gmail.com>
2022-05-21 20:51:05 +0300

    about

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,14 @@
 import AboutSection from "../src/components/AboutSection";
-import FixedImg from "../src/components/FixedImg";
 import HomeSection from "../src/components/HomeSection";
 import Navbar from "../src/components/Navbar";
 import OurServices from "../src/components/OurServices";
 
 export default function Home() {
   return (
-    <div style={{direction:"rtl"}} >
+    <div style={{ direction: "rtl" }}>
       <Navbar />
       <HomeSection />
       <AboutSection />
-      <FixedImg />
       <OurServices />
     </div>
   );
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -3,28 +3,41 @@ import { aboutText } from "../utils/text";
 
 const AboutSection = () => {
   return (
-    <main className="py-16 container" name="about">
-      <div className="text-center">
-        <h2 className="text-primary text-lg font-bold">
-          نحن نقدم منتجات رقمية جميلة
-        </h2>
-        <div className="text-2xl text-gray-700 pt-2 ">
-          <p>نحن نقدم جودة عالية وفعالة من حيث التكلفة في الخارج</p>
-          <p>خدمات تطوير الويب والبرمجيات</p>
-        </div>
-      </div>
-      <div className="grid grid-cols-3 pt-16 place-items-center">
-        {aboutText.map(({ icon, title, description }) => (
-          <div className="flex gap-5   ">
-            <div>{icon}</div>
-            <div className="bg-primary h-6 w-1"></div>
-            <div>
-              <h3 className="text-gray-800 font-medium text-lg">{title}</h3>
-              <p className="text-gray-700">{description}</p>
-            </div>
+    <main name="about">
+      <section className="py-24 container">
+        <div className="text-center">
+          <h2 className="text-primary text-lg font-bold">
+            نحن نقدم منتجات رقمية جميلة
+          </h2>
+          <div className="text-2xl text-gray-700 pt-2 ">
+            <p>نحن نقدم جودة عالية وفعالة من حيث التكلفة في الخارج</p>
+            <p>خدمات تطوير الويب والبرمجيات</p>
           </div>
-        ))}
-      </div>
+        </div>
+        <div className="grid grid-cols-3 pt-16 place-items-center">
+          {aboutText.map(({ icon, title, description }) => (
+            <div className="flex gap-5   ">
+              <div>{icon}</div>
+              <div className="bg-primary h-6 w-[1px]"></div>
+              <div>
+                <h3 className="text-gray-800 font-medium text-lg">{title}</h3>
+                <p className="text-gray-700">{description}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+      <section className="bg-[url('/images/bg.jpg')] flex justify-center items-center  relative  bg-cover bg-fixed  h-[600px] ">
+        <div className="bg-black w-full h-full opacity-50 absolute  "></div>
+        <div className=" relative  container text-center    ">
+          <h2 className="text-primary text-xl">
+            نص تجريبي يحتوي على بعض النصوص التي تستخدم في التصميم.
+          </h2>
+          <p className="text-white text-3xl">
+            نص تجريبي يحتوي على بعض النصوص التي تستخدم في التصميم.
+          </p>
+        </div>
+      </section>
     </main>
   );
 };
