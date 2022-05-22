f7efce4385d833b25422b183fb89ce911fe8118f
m-aljamal <mohammadjamol@gmail.com>
2022-05-22 11:42:31 +0300

    services

--- src/components/OurServices.js
+++ src/components/OurServices.js
@@ -3,10 +3,10 @@ import { services } from "../utils/text";
 
 const OurServices = () => {
   return (
-    <section className="bg-gray-100 py-20 " name="services">
+    <section className="bg-gray-50 py-20 " name="services">
       <div className="container">
         <div className=" pb-10 text-center">
-          <h2 className=" font-medium text-3xl">خدماتنا</h2>
+          <h2 className="font-medium text-3xl text-gray-700">خدماتنا</h2>
           <p className="text-gray-600 w-3/4 mx-auto  mt-4">
             Deserunt irure consectetur magna veniam laboris aute excepteur. Nisi
             mollit do aute dolor. Dolore magna laborum ipsum nulla magna aliqua.
@@ -16,12 +16,20 @@ const OurServices = () => {
         </div>
         <div className="grid grid-cols-4 gap-8 ">
           {services.map(({ id, description, icon, title }) => (
-            <div key={id} className="border p-8 text-center rounded-md    ">
+            <div
+              key={id}
+              className="border p-8 text-center rounded-md bg-white border-b-primary
+               hover:bg-gray-700
+                hover:text-white
+                text-gray-600
+                transition duration-150 ease-out hover:scale-110"
+            >
               <div className="bg-primary w-20 h-20 flex justify-center items-center rounded-full mx-auto">
                 {icon}
               </div>
-              <h2 className=" pt-8 pb-2 font-normal text-lg">{title}</h2>
-              <p className="text-gray-600">{description}</p>
+
+              <h2 className=" pt-8 pb-2 font-semibold text-lg">{title}</h2>
+              <p>{description}</p>
             </div>
           ))}
         </div>
