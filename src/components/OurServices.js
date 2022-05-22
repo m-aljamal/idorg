bb9ab0bc32d3b50fea45614180ff40a3b8f6552d
m-aljamal <mohammadjamol@gmail.com>
2022-05-22 22:08:36 +0300

    class

--- src/components/OurServices.js
+++ src/components/OurServices.js
@@ -106,7 +106,7 @@ const Accordion = ({ id, title, content, setOpen, openId }) => {
         ) : (
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            class="h-6 w-6"
+            className="h-6 w-6"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
